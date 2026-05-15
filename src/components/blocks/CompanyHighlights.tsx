71fc34810c4fed4a993ceaff26dd67e898cb172c
Ippokratis Geranakis <igeran@cmt.local>
2026-05-15 11:56:56 +0300

    Company Highlights full height fixed

--- src/components/blocks/CompanyHighlights.tsx
+++ src/components/blocks/CompanyHighlights.tsx
@@ -30,39 +30,40 @@ export function CompanyHighlights({
           </h2>
         )}
 
-        <div className="w-full px-6 md:px-10 lg:px-16 xl:px-20   bg-white grid grid-cols-1 border-t border-[#d9d9d9] md:grid-cols-3">
+        <div className="w-full px-6 md:px-10 lg:px-16 xl:px-20 bg-white grid grid-cols-1 border-t border-[#d9d9d9] md:grid-cols-3 md:min-h-[65vh] lg:min-h-[60vh]">
           {items?.map((item, index) => (
             <article
               key={item.id ?? index}
               className="
-                flex min-h-[260px] flex-col
+                flex h-full min-h-[260px] flex-col
                 px-6 pt-6 pb-5
                 md:min-h-[258px] md:px-[18px] md:pt-[24px] md:pb-[18px]
                 md:border-r md:border-[#d9d9d9]
-                last:md:border-r-0 items-align-center
+                last:md:border-r-0
+                items-align-center
               "
             >
               {item.icon && (
-              <StrapiImage
-                src={item.icon.url}
-                alt={item.icon.alternativeText || "icon"}
-                width={60}
-                height={60}
-              />
+                <StrapiImage
+                  src={item.icon.url}
+                  alt={item.icon.alternativeText || "icon"}
+                  width={60}
+                  height={60}
+                />
               )}
 
               <div className="mt-auto">
-                <h3 className="font-agenda-medium text-[30px] leading-[0.95] tracking-[-0.04em] text-black md:text-[38px]">
+                <h3 className="font-agenda-regular text-[30px] leading-[0.95] tracking-[-0.04em] text-black md:text-8xl">
                   {item.value}
                 </h3>
 
                 <div className="mt-[8px] space-y-0">
-                  <p className="font-agenda-light text-[13px] uppercase leading-[1.05] tracking-[-0.03em] text-[#3c3c3c] md:text-[18px]">
+                  <p className="font-agenda-regular text-[13px] uppercase leading-[1.05] tracking-[-0.03em] text-[#3c3c3c] md:text-4xl">
                     {item.title}
                   </p>
 
                   {item.subtitle && (
-                    <p className="font-agenda-light text-[13px] uppercase leading-[1.05] tracking-[-0.03em] text-[#3c3c3c] md:text-[18px]">
+                    <p className="font-agenda-regular text-[13px] uppercase leading-[1.05] tracking-[-0.03em] text-[#3c3c3c] md:text-4xl">
                       {item.subtitle}
                     </p>
                   )}
