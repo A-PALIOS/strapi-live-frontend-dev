00df5371e460d40131a82e93d8f142d1413242b0
Ilias Zampetakis <iliaszampetakis@Iliass-MacBook-Air.local>
2026-07-22 00:25:16 +0300

    dashboards bug build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["dashboards.cmtprooptiki.gr/**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
